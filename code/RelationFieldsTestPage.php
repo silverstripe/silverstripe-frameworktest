660fd19a490d89c53eec3cf36a70972ca5fbc57e
Sam Minnee <sam@silverstripe.com>
2009-01-30 05:01:28 +0000

    Added more relation editing fields to frameworktest

--- code/RelationFieldsTestPage.php
+++ code/RelationFieldsTestPage.php
@@ -1,11 +1,14 @@
 <?php
 
 class RelationFieldsTestPage extends TestPage {
+	static $has_one = array(
+		"FavouriteItem" => "TestCTFItem",
+	);
+	static $has_many = array(
+		"Items" => "TestCTFItem",
+	);
 	static $many_many = array(
 		"CheckboxSet" => "TestCategory",
-	);
-	static $has_may = array(
-		"Items" => "TestCTFItem",
 	);
 	
 	function getCMSFields() {
@@ -14,10 +17,27 @@ class RelationFieldsTestPage extends TestPage {
 		$fields->addFieldToTab("Root.Content.CheckboxSet", 
 			new CheckboxSetField("CheckboxSet", "CheckboxSetField", TestCategory::map()));
 
+		/*
 		$fields->addFieldToTab("Root.Content.ComplexTableField", 
 			new ComplexTableField($this, "Items", "TestCTFItem", array(
 				"Title" => "Item Title", 
 				"Author" => "Item Author")));
+			*/
+
+		$fields->addFieldToTab("Root.Content.HasOneComplexTableField", 
+			new HasOneComplexTableField($this, "FavouriteItem", "TestCTFItem", array(
+				"Title" => "Item Title", 
+				"Author" => "Item Author")));
+
+		$fields->addFieldToTab("Root.Content.HasManyComplexTableField", 
+			new HasManyComplexTableField($this, "Items", "TestCTFItem", array(
+				"Title" => "Item Title", 
+				"Author" => "Item Author")));
+
+		$fields->addFieldToTab("Root.Content.ManyManyComplexTableField", 
+			new ManyManyComplexTableField($this, "CheckboxSet", "TestCategory", array(
+				"Title" => "Item Title")));
+
 
 //		$fields->addFieldToTab("Root.Tests.ComplexTableField", 
 //			new CheckboxSetField("CheckboxSet", "CheckboxSetField", TestCategory::map()));
