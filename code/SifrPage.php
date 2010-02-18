33511a8b5c3f08e2e4e7830e49b2c2d96955a470
Saophalkun Ponlu <phalkunz@silverstripe.com>
2010-02-18 21:34:24 +0000

    FEATURE: added a literal in the cms to show the sifr sample image MINOR: tells sifr to use the new font

--- code/SifrPage.php
+++ code/SifrPage.php
@@ -1,6 +1,13 @@
 <?php 
 class SifrPage extends Page {
 	
+	function getCMSFields() {
+		$fields = parent::getCMSFields();
+		$fields->addFieldToTab("Root.Content.SifrSampleImage", new LiteralField("SifrSampleImage", '<p><img src="frameworktest/images/sifr_sample.png"/></p>'));
+		
+		return $fields;
+	}
+	
 }
 
 class SifrPage_Controller extends Page_Controller {
@@ -8,10 +15,10 @@ class SifrPage_Controller extends Page_Controller {
 	function init() {
 		parent::init();
 		
-		Sifr::add_font('chunkfive', 'themes/fonts/chunkfive.swf');
-		Sifr::replace_element('chunkfive', 'h2', "'.sIFR-root { text-align: left; color: red;'");
-		Sifr::replace_element('chunkfive', 'h3', "'.sIFR-root { text-align: left; color: red;'");
-		Sifr::replace_element('chunkfive', 'h4', "'.sIFR-root { text-align: left; color: red;'");
+		Sifr::add_font('blackout', 'themes/fonts/blackout.swf');
+		Sifr::replace_element('blackout', 'h2', "'.sIFR-root { text-align: left; color: red;'");
+		Sifr::replace_element('blackout', 'h3', "'.sIFR-root { text-align: left; color: red;'");
+		Sifr::replace_element('blackout', 'h4', "'.sIFR-root { text-align: left; color: red;'");
 		Sifr::activate_sifr();
 	}
 }
