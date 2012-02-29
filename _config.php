796851cad047925b0bfe83430064fa82cf13f0ba
Ingo Schommer <ingo@silverstripe.com>
2012-02-29 12:54:51 +0100

    MINOR File restructuring, grouping module-specific files and excluding them by default in order to make the module useful for testing basic core components

--- _config.php
+++ _config.php
@@ -3,9 +3,10 @@
 Object::add_extension('Member', 'FrameworkTestRole');
 Object::add_extension('Member', 'FileUploadRole');
 
-Object::add_extension('SiteConfig', 'CMSWorkflowSiteConfigDecorator');
-
-CMSWorkflowSiteConfigDecorator::apply_active_config();
+if(class_exists('SiteTreeCMSWorkflow')) {
+	Object::add_extension('SiteConfig', 'CMSWorkflowSiteConfigDecorator');
+	CMSWorkflowSiteConfigDecorator::apply_active_config();
+}
 
 Director::addRules(100, array(
 	'dev/regress/$Action/$ID' => 'FrameworktestRegressSessionAdmin'
