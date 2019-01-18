2f672ae2caf639d41e3de108d4f82ad45e46b532
Guy Marriott <guy@scopey.co.nz>
2019-01-18 13:56:58 +1300

    NEW Adding a default registry page

---
+++ code/TestRegistryPage.php
@@ -0,0 +1,32 @@
+<?php
+
+use SilverStripe\FrameworkTest\Model\TestPage;
+use SilverStripe\ORM\DataObject;
+use SilverStripe\Registry\RegistryPage;
+use SilverStripe\Security\DefaultAdminService;
+use SilverStripe\Security\Member;
+
+if (!class_exists(RegistryPage::class)) {
+    return;
+}
+
+class TestRegistryPage extends RegistryPage
+{
+    public function requireDefaultRecords()
+    {
+        if (!DataObject::get_one(static::class)) {
+            // Try to create common parent
+            $defaultAdminService = DefaultAdminService::singleton();
+            Member::actAs($defaultAdminService->findOrCreateDefaultAdmin(), function () {
+                $page = new static;
+                $page->Title = 'Registry Test Page';
+                $page->ShowInMenus = 0;
+                $parent = TestPage::getOrCreateParentPage();
+                $page->ParentID = $parent->ID;
+                $page->DataClass = TestRegistryDataObject::class;
+                $page->write();
+                $page->publish('Stage', 'Live');
+            });
+        }
+    }
+}
