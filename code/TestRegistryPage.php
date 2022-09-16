63c3550fbbfd88178628225cccf5b6c3e2478bac
Guy Sartorelli <36352093+GuySartorelli@users.noreply.github.com>
2022-09-16 14:57:14 +1200

    FIX Class manifest was still seeing the files (#125)

--- code/TestRegistryPage.php
+++ code/TestRegistryPage.php
@@ -7,26 +7,24 @@ use SilverStripe\Security\DefaultAdminService;
 use SilverStripe\Security\Member;
 
 if (!class_exists(RegistryPage::class)) {
-    return;
-}
-
-class TestRegistryPage extends RegistryPage
-{
-    public function requireDefaultRecords()
+    class TestRegistryPage extends RegistryPage
     {
-        if (!DataObject::get_one(static::class)) {
-            // Try to create common parent
-            $defaultAdminService = DefaultAdminService::singleton();
-            Member::actAs($defaultAdminService->findOrCreateDefaultAdmin(), function () {
-                $page = new static;
-                $page->Title = 'Registry Test Page';
-                $page->ShowInMenus = 0;
-                $parent = TestPage::getOrCreateParentPage();
-                $page->ParentID = $parent->ID;
-                $page->DataClass = TestRegistryDataObject::class;
-                $page->write();
-                $page->copyVersionToStage('Stage', 'Live');
-            });
+        public function requireDefaultRecords()
+        {
+            if (!DataObject::get_one(static::class)) {
+                // Try to create common parent
+                $defaultAdminService = DefaultAdminService::singleton();
+                Member::actAs($defaultAdminService->findOrCreateDefaultAdmin(), function () {
+                    $page = new static;
+                    $page->Title = 'Registry Test Page';
+                    $page->ShowInMenus = 0;
+                    $parent = TestPage::getOrCreateParentPage();
+                    $page->ParentID = $parent->ID;
+                    $page->DataClass = TestRegistryDataObject::class;
+                    $page->write();
+                    $page->copyVersionToStage('Stage', 'Live');
+                });
+            }
         }
     }
 }
