c2cf7f29f5b007ddd3a3a52596444ec94341ec55
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2022-12-13 14:24:21 +1300

    DEP Update webpack config and deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,18 @@
 const Path = require('path');
-const webpackConfig = require('@silverstripe/webpack-config');
+const { JavascriptWebpackConfig } = require('@silverstripe/webpack-config');
 
-const {
-  resolveJS,
-  externalJS,
-  moduleJS,
-  pluginJS,
-} = webpackConfig;
-
-const ENV = process.env.NODE_ENV;
 const PATHS = {
-  MODULES: 'node_modules',
-  FILES_PATH: '../',
   ROOT: Path.resolve(),
   SRC: Path.resolve('client/src'),
-  DIST: Path.resolve('client/dist'),
-  LEGACY_SRC: Path.resolve('client/src'),
 };
 
 const config = [
-  {
-    name: 'js',
-    entry: {
-      legacy: `${PATHS.LEGACY_SRC}/bundles/legacy.js`,
-    },
-    output: {
-      path: PATHS.DIST,
-      filename: 'js/[name].js',
-    },
-    devtool: (ENV !== 'production') ? 'source-map' : '',
-    resolve: resolveJS(ENV, PATHS),
-    externals: externalJS(ENV, PATHS),
-    module: moduleJS(ENV, PATHS),
-    plugins: pluginJS(ENV, PATHS),
-  },
+  // Main JS bundle
+  new JavascriptWebpackConfig('js', PATHS, 'silverstripe/frameworktest')
+    .setEntry({
+      legacy: `${PATHS.SRC}/bundles/legacy.js`,
+    })
+    .getConfig(),
 ];
 
-// Use WEBPACK_CHILD=js or WEBPACK_CHILD=css env var to run a single config
-module.exports = (process.env.WEBPACK_CHILD)
-  ? config.find(entry => entry.name === process.env.WEBPACK_CHILD)
-  : module.exports = config;
+module.exports = config;
