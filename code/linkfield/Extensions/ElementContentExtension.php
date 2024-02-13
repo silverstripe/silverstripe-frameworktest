f8eac086d24071e33e70da90db20206a19a9a733
Sabina Talipova <87288324+sabina-talipova@users.noreply.github.com>
2024-02-13 14:27:18 +1300

    MNT Behat test LinkField page and LinkField block (#162)

---
+++ code/linkfield/Extensions/ElementContentExtension.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace SilverStripe\FrameworkTest\LinkField\Extensions;
+
+use SilverStripe\Core\Extension;
+use SilverStripe\LinkField\Form\LinkField;
+use SilverStripe\LinkField\Form\MultiLinkField;
+use SilverStripe\LinkField\Models\Link;
+use SilverStripe\LinkField\Models\EmailLink;
+use SilverStripe\LinkField\Models\PhoneLink;
+use SilverStripe\LinkField\Models\SiteTreeLink;
+
+class ElementContentExtension extends Extension
+{
+
+    private static bool $inline_editable = true;
+
+    private static array $has_one = [
+        'OneLink' => Link::class,
+    ];
+
+    private static $has_many = [
+        'ManyLinks' => Link::class . '.Owner',
+    ];
+
+    private static array $owns = [
+        'OneLink',
+        'ManyLinks',
+    ];
+
+    private static array $cascade_deletes = [
+        'OneLink',
+        'ManyLinks',
+    ];
+
+    private static array $cascade_duplicates = [
+        'OneLink',
+        'ManyLinks',
+    ];
+
+    public function updateCMSFields($fields)
+    {
+        $fields->removeByName(['OneLinkID', 'ManyLinks']);
+        $fields->addFieldsToTab(
+            'Root.Main',
+            [
+                LinkField::create('OneLink')
+                    ->setAllowedTypes([
+                        SiteTreeLink::class,
+                        EmailLink::class,
+                        PhoneLink::class
+                    ]),
+                MultiLinkField::create('ManyLinks'),
+            ],
+        );
+    }
+}
