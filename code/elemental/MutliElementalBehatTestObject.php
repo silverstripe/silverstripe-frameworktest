579e1602e6f8b28251f9182a474e5ce32b051df0
Guy Sartorelli <36352093+GuySartorelli@users.noreply.github.com>
2024-07-29 11:45:23 +1200

    FIX Don't require classes that don't exist (#195)

--- code/elemental/MutliElementalBehatTestObject.php
+++ code/elemental/MutliElementalBehatTestObject.php
@@ -5,6 +5,10 @@ namespace SilverStripe\FrameworkTest\Elemental\Model;
 use SilverStripe\ORM\DataObject;
 use SilverStripe\FrameworkTest\Elemental\Extension\MultiElementalAreasExtension;
 
+if (!class_exists(MultiElementalAreasExtension::class)) {
+    return;
+}
+
 class MultiElementalBehatTestObject extends DataObject
 {
     private static $db = [
@@ -17,22 +21,22 @@ class MultiElementalBehatTestObject extends DataObject
         MultiElementalAreasExtension::class,
     ];
 
-    public function canView($member = null) 
+    public function canView($member = null)
     {
         return true;
     }
 
-    public function canEdit($member = null) 
+    public function canEdit($member = null)
     {
         return true;
     }
 
-    public function canDelete($member = null) 
+    public function canDelete($member = null)
     {
         return true;
     }
 
-    public function canCreate($member = null, $context = []) 
+    public function canCreate($member = null, $context = [])
     {
         return true;
     }
